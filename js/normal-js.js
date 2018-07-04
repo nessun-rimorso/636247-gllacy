7bc392bfe176d3146ae6084294c49f491f6c735d
nessun-rimorso <7planeta@bk.ru>
2018-07-04 22:11:22 +0300

    финал +

--- js/normal-js.js
+++ js/normal-js.js
@@ -1,26 +1,77 @@
 // no-js
 
-document.documentElement
-    .classList.replace('no-js', 'js');
+var check = document.querySelector(".no-js");
+
+check.classList.add("js");
+check.classList.remove("no-js");
 
 // popup
 
 var link = document.querySelector(".feedback-btn");
-var popup = document.querySelector(".modal-overlay");
+var overlay = document.querySelector(".modal-overlay");
+var popup = document.querySelector(".modal-popup");
 var close = popup.querySelector(".popup-close");
 var userName = document.querySelector(".popup-name");
+var login = document.querySelector(".popup-email");
+var comment = document.querySelector(".popup-comment");
+var form = document.querySelector(".popup-form");
+var isStorageSupport = true;
+var storage = "";
+
+try {
+    storage = localStorage.getItem("login");
+} catch (err) {
+    isStorageSupport = false;
+}
   
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
+    overlay.classList.add("overlay-show");
     popup.classList.add("modal-show");
-    userName.focus();
+    if (storage) {
+        login.value = storage;
+        userName.focus();
+    } else {
+        userName.focus();
+    }
 });
   
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
+    overlay.classList.remove("overlay-show");
     popup.classList.remove("modal-show");
 });
 
+form.addEventListener("submit", function (evt) {
+     if (!userName.value || !login.value || !comment.value) {
+        evt.preventDefault();
+        popup.classList.remove("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("login", login.value);
+      }
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        if (popup.classList.contains("modal-show")) {
+            evt.preventDefault();
+            overlay.classList.remove("overlay-show");
+            popup.classList.remove("modal-show");
+            popup.classList.remove("modal-error");
+        }
+    }
+});
+
+overlay.addEventListener("click", function (evt) {
+    overlay.classList.remove("overlay-show");
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+});
+    
 // slider
 
 var background = document.querySelector(".site-background");
